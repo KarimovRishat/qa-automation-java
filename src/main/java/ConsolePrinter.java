c71d327f0d8540039f35102ede6cc48e6ce386a5
richskiter <richskiter@gmail.com>
2022-03-22 11:57:48 +0300

    docs: add ConsolePrinter documentation

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,5 +1,15 @@
+/**
+ * <p>Класс для вывода сообщения<p/>
+ * @author Каримов Ришат
+ */
 public class ConsolePrinter {
+    /**
+     * <p>Метод выводит массив типа String<p/>
+     * @param message - string message (сообщение со строковым типом)
+     * @affects print - none (нет побочных эффектов)
+     */
     public static void print(String message) {
+
         System.out.println(message);
     }
 }
