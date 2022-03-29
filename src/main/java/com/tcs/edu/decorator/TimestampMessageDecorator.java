f8560982e325e94313a6207f104a4fcc442e1b64
richskiter <richskiter@gmail.com>
2022-03-29 14:31:27 +0300

    feat: Добавлена переменная messageCount, определяющая порядковый номер выводимого на экран сообщения

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -1,19 +1,25 @@
 package com.tcs.edu.decorator;
 
 import java.time.Instant;
+
 /**
  * <p>Класс для выдачи текущей даты и времени<p/>
  * @author Каримов Ришат
  */
 public class TimestampMessageDecorator {
     /**
+     * <p>Глобальная переменная, присваивающая порядковые номера сообщениям</p>
+     */
+    public static int messageCount = 0;
+
+    /**
      * <p>Метод возвращает массив типа String<p/>
      * @param message - string message (сообщение со строковым типом)
      * @return - возвращает переменную decoratedMessage (текущая дата и время + сообщение)
      */
     public static String decorate(String message) {
-
-        final String decoratedMessage = Instant.now() + " " + message;
+        messageCount++;
+        final String decoratedMessage = messageCount+ " " + Instant.now() + " " + message;
         return decoratedMessage;
     }
 }
