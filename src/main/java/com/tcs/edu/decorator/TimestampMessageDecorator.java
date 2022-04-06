55bcd726d0ad4d5ac07729d9e3877b030428e553
richskiter <richskiter@gmail.com>
2022-04-06 12:45:39 +0300

    feat: Добавлено разделение на страницы каждые 2 сообщения style: Удалены лишние переходы строк

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 /**
  * <p>Класс для выдачи текущей даты и времени<p/>
+ *
  * @author Каримов Ришат
  */
 public class TimestampMessageDecorator {
@@ -13,14 +14,26 @@ public class TimestampMessageDecorator {
     public static int messageCount = 0;
 
     /**
+     * <p>Глобальная переменная, декларирующая размер страницы</p>
+     */
+    public static final int PAGE_SIZE = 2;
+
+    /**
      * <p>Метод возвращает массив типа String<p/>
+     *
      * @param message - string message (сообщение со строковым типом)
      * @return - возвращает переменную decoratedMessage (текущая дата и время + сообщение)
-     * Переменная decoratedMessage использует форматирование строк по шаблону
+     * Переменная decoratedMessage использует форматирование строк по шаблону (шаблон заведен
+     * как переменная template)
+     * Каждые 2 сообщения текст делится на страницы
      */
     public static String decorate(String message) {
         messageCount++;
-        final var decoratedMessage = String.format("%d %s %s", messageCount, Instant.now(), message);
+        String template = "%d %s %s";
+        if (messageCount % PAGE_SIZE == 0) {
+            template += "\n ______";
+        }
+        final var decoratedMessage = String.format(template, messageCount, Instant.now(), message);
         return decoratedMessage;
     }
 }
--- src/main/java/com/tcs/edu/printer/ConsolePrinter.java
+++ src/main/java/com/tcs/edu/printer/ConsolePrinter.java
@@ -1,13 +1,16 @@
 package com.tcs.edu.printer;
+
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 
 /**
  * <p>Класс для вывода сообщения<p/>
+ *
  * @author Каримов Ришат
  */
 public class ConsolePrinter {
     /**
      * <p>Метод выводит массив типа String<p/>
+     *
      * @param message - string message (сообщение со строковым типом)
      * @affects print - none (нет побочных эффектов)
      */
